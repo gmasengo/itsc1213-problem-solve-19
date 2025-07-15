80dc1cce85cd5e0d0178fd921932b6a6a4140b4b
gmasengo <guylain2010@gmail.com>
2025-07-15 15:59:43 -0400

    updated my rectangle code
    updated and tested my rectangle problem

--- RectangleTest.java
+++ RectangleTest.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
 import java.awt.Rectangle;
 
+
 public class RectangleTest {
+    // // Inside main() 
+ Scanner input = new Scanner(System.in);
+ System.out.print("Enter width: "); 
+ int width = input.nextInt();
+ System.out.print("Enter height: "); 
+ int height = input.nextInt();
+ 
     public static void main(String[] args) {
         Rectangle box1 = new Rectangle(10, 10, 40, 30);
         System.out.println("box1: " + box1);
     }
 
 }
+//
